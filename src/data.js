a9e98c7fe7b0d2af883efcb1bb0bf52573e8b6d7
Cheilanthe <65095040+Cheilanthe@users.noreply.github.com>
2023-01-11 12:05:07 -0600

    pprimera versión de Eli

--- src/data.js
+++ src/data.js
@@ -1,8 +1,29 @@
 // estas funciones son de ejemplo
+//aquí voy a  contruir las funciones para manipular mis datos.
+//primero hay que hacer una función que me filtre mis datos
+//quiero filtrar datos por nombre. Primero utilizaré el método filter
 
-export const example = () => {
-  return 'example';
-};
+
+export function namePokemon (data, name) {
+  const namePoke= name;
+  for (let i = 0; i< data.length; i++){
+    console.log(data[i].name)
+    if(data[i].name === namePoke){
+      let nombre = namePoke;
+      return nombre;
+    }else{
+      return "incorrecto"
+    }
+  }
+}
+
+/*export function name (data, nameP){
+  data.filter ( pokemon => pokemon.name === nameP);
+}*/
+
+//quiero filtrar datos por habilidad y debilidad 
+
+
 
 export const anotherExample = () => {
   return 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import {namePokemon} from './data.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const pokemones = data.pokemon; //creamos un variable que iba a contener los datos para poder acceder a ellos. 
+const botonSearch = document.getElementById("search");
+botonSearch.addEventListener("click", botonName); 
+function botonName(event){
+  event.preventDefault();
+  const inputName = document.querySelector("name");
+  const nombreFiltrado = namePokemon(pokemones, inputName);//aquí probamos la función
+  console.log(`Tu pokemon es ${nombreFiltrado}`);
+}
+
+
+/*for (let i = 0; i< pokemones.length; i++){
+    console.log(pokemones[i])
+}este es una prueba*/
+
+
+
+
+
