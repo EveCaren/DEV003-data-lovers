8ee995b9dde100af1bd6de232593164c5a4f6f76
Cheilanthe <65095040+Cheilanthe@users.noreply.github.com>
2023-01-19 12:14:47 -0600

    agrega el template, agrega función ordenar y muestra el filtro name

--- src/data.js
+++ src/data.js
@@ -1,13 +1,15 @@
 //aquí voy a  contruir las funciones para manipular mis datos.
 //primero hay que hacer una función que me filtre mis datos por nombre
 export function namePokemon (data, name) {
+  const filtroName = [];
   const namePoke= name;
   for (let i = 0; i< data.length; i++){
     if(data[i].name === namePoke){
-      return(data[i]);
+      filtroName.push(data[i]);
       
     }
   }
+  return filtroName
 }
 
 //filtrar por habilidad 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {namePokemon, abcOrder} from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
+
+// este es el evento que busca un pokemon por nombre
 const pokemones = data.pokemon; //creamos un variable que iba a contener los datos para poder acceder a ellos. 
 const botonSearch = document.getElementById("search");
 botonSearch.addEventListener("click", botonName); 
@@ -9,36 +12,42 @@ function botonName(event){
   const inputName = document.getElementById("name").value;
   const nombreFiltrado = namePokemon(pokemones, inputName);//aquí probamos la función
   console.log(nombreFiltrado);
+  if (nombreFiltrado.length === 0){
+    console.log("No existe")
+  }else{
+    const $cards = document.querySelector(".section3"),
+      $template = document.getElementById("template__card").content,
+      $fragment = document.createDocumentFragment();
 
+    nombreFiltrado.forEach(el =>{
+      $template.querySelector("img").setAttribute("src",el.img);
+      $template.querySelector("img").setAttribute("alt",el.num);
+      $template.querySelector("figcaption").textContent =el.name;
+      let $clone = document.importNode($template,true);
+      $fragment.appendChild($clone);
+    });
 
-  const card = `
-  <div class ="card">
-    <h3>${nombreFiltrado.name}</h3>
-  </div>
-  `;
-  document.body.innerHTML = card;
+    $cards.appendChild($fragment);
+  }
 }
 
-/*for (let i = 0; i< pokemones.length; i++){
-    console.log(pokemones[i])
-}este es una prueba*/
+//Orden alfabético
+const ordenar= document.getElementById("orderSelect");
+const selected= ordenar.options[ordenar.selectedIndex].text;  
+const ordenpokemones = abcOrder(pokemones,selected);
+const $cards = document.querySelector(".section3"),
+  $template = document.getElementById("template__card").content,
+  $fragment = document.createDocumentFragment();
 
-//filtro por nombre
+ordenpokemones.forEach(el =>{
+  $template.querySelector("img").setAttribute("src",el.img);
+  $template.querySelector("img").setAttribute("alt",el.num);
+  $template.querySelector("figcaption").textContent =el.name;
+  let $clone = document.importNode($template,true);
+  $fragment.appendChild($clone);
+});
+$cards.appendChild($fragment);
 
-const ordenpokemones = abcOrder(pokemones,'A-Z');
-console.log(ordenpokemones);
 
-/*const orden= pokemones.sort((a,b) => {
-  const nameA= a.name.toLowerCase();
-  const nameB= b.name.toLowerCase();
-  if (nameA<nameB){
-    return-1;
-  }
-  if (nameA>nameB){
-    return 1;
-  }
-  return 0;
-})
-console.log(orden);*/
 
 
