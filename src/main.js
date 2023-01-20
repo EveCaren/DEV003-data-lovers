979efb01b6e0e5f82b1a018a9d971688bceb7fb1
EveCaren <eufracio.evelin@gmail.com>
2023-01-19 19:27:01 -0500

    cambios html, css y  js jueves 19

--- src/main.js
+++ src/main.js
@@ -9,26 +9,32 @@ const botonSearch = document.getElementById("search");
 botonSearch.addEventListener("click", botonName); 
 function botonName(event){
   event.preventDefault();
+
   const inputName = document.getElementById("name").value;
   const nombreFiltrado = namePokemon(pokemones, inputName);//aquí probamos la función
   console.log(nombreFiltrado);
+  const $cards = document.querySelector(".section3"),
+  $template = document.getElementById("template__card").content,
+  $fragment = document.createDocumentFragment();
+  $cards.innerHTML = "";
   if (nombreFiltrado.length === 0){
-    console.log("No existe")
-  }else{
-    const $cards = document.querySelector(".section3"),
-      $template = document.getElementById("template__card").content,
-      $fragment = document.createDocumentFragment();
+    $template.querySelector("img").setAttribute("src","./img/pikachu.png");
+    $template.querySelector("img").setAttribute("alt","pikachu");
+    $template.querySelector("p").textContent ="Lo sentimos, no hay pokemones con esas características.";
+    let $clone = document.importNode($template,true);
+    $fragment.appendChild($clone);
 
+  }else{
     nombreFiltrado.forEach(el =>{
       $template.querySelector("img").setAttribute("src",el.img);
       $template.querySelector("img").setAttribute("alt",el.num);
+      $template.querySelector("p").textContent =`No. ${el.num}`;
       $template.querySelector("figcaption").textContent =el.name;
       let $clone = document.importNode($template,true);
       $fragment.appendChild($clone);
     });
-
-    $cards.appendChild($fragment);
   }
+  $cards.appendChild($fragment);
 }
 
 //Orden alfabético
@@ -42,12 +48,30 @@ const $cards = document.querySelector(".section3"),
 ordenpokemones.forEach(el =>{
   $template.querySelector("img").setAttribute("src",el.img);
   $template.querySelector("img").setAttribute("alt",el.num);
+  $template.querySelector("p").textContent =`No. ${el.num}`;
   $template.querySelector("figcaption").textContent =el.name;
   let $clone = document.importNode($template,true);
   $fragment.appendChild($clone);
 });
 $cards.appendChild($fragment);
 
+// Card características
+const buttonPag2 = document.getElementById("feature");
+buttonPag2.addEventListener("click",characteristics);
+function characteristics(e){
+  e.preventDefault();
+  const $cards = document.querySelector(".section3"),
+    $template = document.getElementById("card__pokemon").content,
+    $fragment = document.createDocumentFragment();
 
+  pokemones.forEach(el =>{
+    $template.querySelector("h1").textContent =el.name;
+    $template.querySelector("img").setAttribute("src",el.img);
+    $template.querySelector("img").setAttribute("alt",el.num);
+    $template.querySelector("p").textContent =`No. ${el.num}`;
+    let $clone = document.importNode($template,true);
+    $fragment.appendChild($clone);
+  });
 
-
+  $cards.appendChild($fragment);
+}
