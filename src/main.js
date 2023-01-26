eddda0dbc307c47d9782283ad54549e09ec946f1
EveCaren <eufracio.evelin@gmail.com>
2023-01-25 20:08:15 -0500

    cambios miercoles25, main.js y html

--- src/main.js
+++ src/main.js
@@ -2,22 +2,32 @@ import {namePokemon, idPokemon, abcOrder} from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 const pokemones = data.pokemon; //creamos un variable que iba a contener los datos para poder acceder a ellos.
-//orden por número 
-const $container = document.querySelector(".section3"),
-  $template = document.getElementById("template__card").content,
-  $fragment = document.createDocumentFragment();
-pokemones.forEach(el =>{
-  $template.querySelector("img").setAttribute("src",el.img);
-  $template.querySelector("img").setAttribute("alt",el.num);
-  $template.querySelector("p").textContent =`No. ${el.num}`;
-  $template.querySelector("figcaption").textContent =el.name;
-  $template.querySelector(".feature").dataset.id = el.num;
+//función que muestra las cards
+function drawCards(pokemon){
+  const $container = document.querySelector(".section3"),
+    $template = document.getElementById("template__card").content,
+    $fragment = document.createDocumentFragment();
+  $template.querySelector("img").setAttribute("src",pokemon.img);
+  $template.querySelector("img").setAttribute("alt",pokemon.num);
+  $template.querySelector("p").textContent =`No. ${pokemon.num}`;
+  $template.querySelector("figcaption").textContent =pokemon.name;
+  $template.querySelector(".feature").dataset.id = pokemon.num;
+
   let $clone = document.importNode($template,true);
   $fragment.appendChild($clone);
-});
-$container.appendChild($fragment);
+  $container.appendChild($fragment);
+  const feature = document.querySelector(".feature");
+  console.log(feature);
+  // for( let i=0; i<feature.length; i++){
+  //   i.addEventListener("click",e =>{console.log(e.target)});
+  // }
+  
+  // feature.forEach((el)=> el.addEventListener("click",e =>{console.log(e.target)}));
+}
+pokemones.forEach(pokemon=>{drawCards(pokemon)})
 
 //evento de los botones características
+
 const botones = document.querySelectorAll(".feature");
 botones.forEach((el)=> el.addEventListener("click", e=>{
   const idBoton = e.target.getAttribute("data-id");
@@ -35,8 +45,8 @@ botones.forEach((el)=> el.addEventListener("click", e=>{
     $template.querySelector("p").textContent =`No. ${el.num}`;
     $template.querySelector(".group2").textContent =`
         Height:${el.size.height} 
-        Weight: ${el.size.weight}`;
-    //Pokemon Rarity: ${el.pokemonrarity}
+        Weight: ${el.size.weight}
+        Pokemon Rarity: ${el["pokemon-rarity"]}`;
     $template.querySelector(".group3").textContent =`
         About:${el.about}`;
     $template.querySelector(".group4").textContent =`
@@ -57,14 +67,19 @@ function botonName(event){
   event.preventDefault();
 
   const inputName = document.getElementById("name").value;
-  const nombreFiltrado = namePokemon(pokemones, inputName);//aquí probamos la función
-  console.log(nombreFiltrado);
-  const $container = document.querySelector(".section3"),
+  const pokemon = namePokemon(pokemones, inputName);//aquí probamos la función
+  console.log(pokemon);
+  // const $container = document.querySelector(".section3"),
+  // $template = document.getElementById("template__card").content,
+  // $template2 = document.getElementById("noFound").content,
+  // $fragment = document.createDocumentFragment();
+  // $container.innerHTML = "";
+  if (pokemon.length === 0){
+    const $container = document.querySelector(".section3"),
     $template = document.getElementById("template__card").content,
     $template2 = document.getElementById("noFound").content,
     $fragment = document.createDocumentFragment();
   $container.innerHTML = "";
-  if (nombreFiltrado.length === 0){
     $template2.querySelector("img").setAttribute("src","./img/pikachu.png");
     $template2.querySelector("img").setAttribute("alt","pikachu");
     $template2.querySelector("figcaption").textContent ="Lo sentimos, no hay pokemones con esas características.";
@@ -72,18 +87,21 @@ function botonName(event){
     $container.appendChild($clone);
 
   }else{
-    nombreFiltrado.forEach(el =>{
-      $template.querySelector("img").setAttribute("src",el.img);
-      $template.querySelector("img").setAttribute("alt",el.num);
-      $template.querySelector("p").textContent =`No. ${el.num}`;
-      $template.querySelector("figcaption").textContent =el.name;
-      $template.querySelector(".feature").dataset.id = el.num;
-      let $clone = document.importNode($template,true);
-      $fragment.appendChild($clone);
-    });
-  }
-  $container.appendChild($fragment);
-}
+    pokemon.forEach(pokemon =>{
+      const $container = document.querySelector(".section3");
+      $container.innerHTML = "";
+      drawCards(pokemon);
+    //   $template.querySelector("img").setAttribute("src",el.img);
+    //   $template.querySelector("img").setAttribute("alt",el.num);
+    //   $template.querySelector("p").textContent =`No. ${el.num}`;
+    //   $template.querySelector("figcaption").textContent =el.name;
+    //   $template.querySelector(".feature").dataset.id = el.num;
+    //   let $clone = document.importNode($template,true);
+    //   $fragment.appendChild($clone);
+    // });
+    })
+  // $container.appendChild($fragment);
+}}
 
 //filtro del Orden alfabético
 
